d0ebbd2c1dbcd166e223c92b88ae82df1a54e4dc
Arjesh <arjesh.khadka2@gmail.com>
2023-11-11 12:27:46 +1100

    feat: new screen added for drawer navigation

--- App.js
+++ App.js
@@ -5,8 +5,20 @@ import { NavigationContainer } from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack'
 import MealsOverviewScreen from './src/screens/MealsOverviewScreen';
 import MealDetailScreen from './src/screens/MealDetailScreen';
+// import {createDrawerNavigator} from '@react-navigation/drawer'
+import FavouritesScreen from './src/screens/FavouritesScreen';
 
 const Stack = createNativeStackNavigator();
+// const Drawer = createDrawerNavigator()
+
+// function DrawerNavigator(){
+//   return(
+//     <Drawer.Navigator>
+//       <Drawer.Screen name="Categories" component={CategoriesScreen}/>
+//       <Drawer.Screen name="Favourites" component={FavouritesScreen}/>
+//     </Drawer.Navigator>
+//   )
+// }
 export default function App() {
   return (
     <>
---
+++ src/screens/FavouritesScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const FavouritesScreen = () => {
+  return (
+    <View>
+        <Text>
+            FavouritesScreen
+        </Text>
+      
+    </View>
+  )
+}
+
+export default FavouritesScreen
