d09cc445462bc4ed2088093c5aed8d6c84b314d4
Arjesh <arjesh.khadka2@gmail.com>
2023-11-14 21:57:16 +1100

    feat: favourite meal data extracted

--- src/screens/FavouritesScreen.js
+++ src/screens/FavouritesScreen.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Text, View } from 'react-native'
+import { FavouriteContext } from '../../store/context/favourites_context';
+import { MEALS } from '../../data/dummyData';
 
 const FavouritesScreen = () => {
+    const favouriteMealContext = useContext(FavouriteContext);
+    const [selectedFavourite, setSelectedFavourite] = useState([])
+
+    useEffect(()=>{
+      favouriteMealContext.ids.map((item) => {
+      const faviouriteMealObj = MEALS.filter((meal)=> meal.id === item )
+      setSelectedFavourite((prev) => [...prev, faviouriteMealObj])
+    })
+    }, [])
+
+    console.log(selectedFavourite)
+
+    
+
+    
+
+    
   return (
     <View>
         <Text>
-            FavouritesScreen
+            FavouritesScreen 
         </Text>
       
     </View>
